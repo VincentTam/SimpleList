37b710070f83328c72642fb411cc7bdb788bea8a
Vincent Tam <sere@live.hk>
2026-02-05 21:53:53 +0100

    Tut3
    CMake plugin couldn't rebuild
    
    ```
    [main] Building folder: /home/vin100/Documents/SimpleList/out/build/default
    [build] Starting build
    [proc] Executing command: /usr/bin/cmake --build /home/vin100/Documents/SimpleList/out/build/default --target SimpleList --
    [build] ninja: no work to do.
    [driver] Build completed: 00:00:00.040
    [build] Build finished with exit code
    ```
    
    ninji says nothing to do
    
    VSCode Chatbot says:
    > The issue is that the CMake extension is using a different build directory than before. Notice the build path in the output:
    > ```
    > /home/vin100/Documents/SimpleList/out/build/default
    > ```
    > but i had been using
    > ```
    > /home/vin100/Documents/SimpleList/build
    > ```
    
    Bot suggested adding `binaryDir`.
    It still didn't work until I changed `[Default]` → `default` under `Build` in CMake plugin.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!" << std::endl;
+void print_menu(string name);
+
+int main(int arg_count, char *args[]) {
+  if (arg_count > 1) {
+    string name(args[1]);
+    print_menu(name);
+  } else {
+    cout << "Username not supplied.. exiting the program" << endl;
+  }
   return 0;
+}
+
+void print_menu(string name) {
+  int choice;
+
+  cout << "****************\n";
+  cout << "1 - Print list.\n";
+  cout << "2 - Add to list.\n";
+  cout << "3 - Delete from list.\n";
+  cout << "4 - Quit.\n";
+
+  cin >> choice;
+
+  if (choice == 4) {
+    exit(0);
+  }
+  else {
+    cout << "Sorry choice not implemented yet.\n";
+  }
 }
